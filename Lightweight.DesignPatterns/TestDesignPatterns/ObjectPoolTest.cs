37225474ebd9b2cfaf0e23e4fb0bb2223abd2534
Unknown <arthur.delamare1@viacesi.fr>
2018-12-02 14:59:36 +0100

    [ADD] Test Method for Get, Release and Count in ObjectPoolTest Class

--- Lightweight.DesignPatterns/TestDesignPatterns/ObjectPoolTest.cs
+++ Lightweight.DesignPatterns/TestDesignPatterns/ObjectPoolTest.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lightweight.DesignPatterns.ObjectPool;
 
 namespace TestDesignPatterns
 {
     [TestClass]
     public class ObjectPoolTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private class Person
         {
+            public string Name { get; set; }
+        }
+
+        ObjectPool<Person> objectPool;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            objectPool = new ObjectPool<Person>(size: 3);
+        }
+
+        [TestMethod]
+        public void GetTest()
+        {
+            Person person = objectPool.Get();
+            Assert.IsNotNull(person);
+            Assert.IsInstanceOfType(person, typeof(Person));
+        }
+
+        [TestMethod]
+        public void ReleaseTest()
+        {
+            Person person = objectPool.Get();
+            Assert.IsNotNull(person);
+            objectPool.Release(person);
+        }
+
+        [TestMethod]
+        public void CountTest()
+        {
+            Person person = objectPool.Get();
+            Assert.IsNotNull(person);
+            Assert.AreEqual(1, objectPool.Count);
         }
     }
 }
