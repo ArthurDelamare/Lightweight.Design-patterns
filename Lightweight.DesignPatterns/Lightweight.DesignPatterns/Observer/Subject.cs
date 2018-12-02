a8577526a5f3235f6657cc446d094fc17b6770ec
Unknown <arthur.delamare1@viacesi.fr>
2018-12-02 15:47:58 +0100

    [ADD] Observer Design Pattern

---
+++ Lightweight.DesignPatterns/Lightweight.DesignPatterns/Observer/Subject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lightweight.DesignPatterns.Observer
+{
+    public class Subject : ISubject
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+
+        public void Notify()
+        {
+            this._observers.ForEach(observer => observer.Update());
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            this._observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            this._observers.Remove(observer);
+        }
+    }
+}
