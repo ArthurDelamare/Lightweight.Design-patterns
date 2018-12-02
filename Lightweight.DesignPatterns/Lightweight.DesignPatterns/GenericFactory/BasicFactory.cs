3b0d46d2d490ac4c4426381cf452cd9bf9dbf02a
Unknown <arthur.delamare1@viacesi.fr>
2018-12-02 16:57:42 +0100

    [ADD] Generic Factory Pattern

---
+++ Lightweight.DesignPatterns/Lightweight.DesignPatterns/GenericFactory/BasicFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lightweight.DesignPatterns.GenericFactory
+{
+    public class BasicFactory : IGenericFactory
+    {
+        public TProduct Create<TProduct>() where TProduct : new()
+        {
+            return new TProduct();
+        }
+    }
+}
