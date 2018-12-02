9399b43ff8292ce836fcdb4b988ca5f9a8cff766
Unknown <arthur.delamare1@viacesi.fr>
2018-12-02 14:36:40 +0100

    [ADD] Empty ObjectPool Test Class

---
+++ Lightweight.DesignPatterns/TestDesignPatterns/ObjectPoolTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDesignPatterns
+{
+    [TestClass]
+    public class ObjectPoolTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
